b6fbfb4e1c8cc52769a0e68cd246d7e3a7a08e1f
Laura Gonzalez <lg458@cornell.edu>
2022-06-15 14:46:35 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+//returns an array of thank you messages for each name provided
+const cards = [];
+
+function writeCards(recipients, eventType) { 
+  for (let i = 0; i < recipients.length; i++) { 
+    cards.push(`Thank you, ${recipients[i]}, for the wonderful ${eventType} gift!`);
+  } 
+  return cards;
+} 
+
+//counts down from the number provided to zero by invoking console.log once for each number
+function countDown(n) { 
+  while (n >= 0) { 
+    console.log(n);
+    n--;
+  }
+}
